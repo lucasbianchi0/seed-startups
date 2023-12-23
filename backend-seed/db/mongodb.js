e687dad2304673ae166723f56c4d42c116aac338
lucas bianchi <lucmbianchi2000@gmail.com>
2023-12-23 07:30:39 -0300

    se agregan modificaciones

--- backend-seed/db/mongodb.js
+++ backend-seed/db/mongodb.js
@@ -1,17 +1,20 @@
-const mongoose  = require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config(); // Cargar variables de entorno desde el archivo .env
 
-const connectDB = async ()=>{
-    try{
-        DB_URI= 'mongodb+srv://luki-project:EKeZBffswBXje7gF@cluster0.fdqdutk.mongodb.net/?retryWrites=true&w=majority',
-        await mongoose.set('strictQuery',false);
-        await mongoose.connect(DB_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology: true,
-    })
-    console.log('mongo conectado')
-    }catch(err){
-        console.log(err)
-    }
-}
+const connectDB = async () => {
+  try {
+    const DB_URI = process.env.DB_URI; // Usar la variable de entorno
 
-module.exports= connectDB
+    await mongoose.set('strictQuery', false);
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB conectado');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = connectDB;
